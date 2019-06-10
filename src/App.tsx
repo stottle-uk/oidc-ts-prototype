fe73c48d159292ea5ae968eac76cedf940439853
Stuart Tottle <stuarttottle@hotmail.com>
2019-06-10 11:00:18 +0100

    Try to authenticate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
+import Oidc, { UserManager, UserManagerSettings } from 'oidc-client';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { globoIdConfig } from './env';
+
+Oidc.Log.logger = console;
+Oidc.Log.level = Oidc.Log.DEBUG;
 
 const App: React.FC = () => {
+  var settings: UserManagerSettings = {
+    authority: globoIdConfig.authority,
+    client_id: globoIdConfig.client_id,
+    redirect_uri: window.location.origin,
+    response_type: 'code',
+    scope: 'openid profile',
+
+    filterProtocolClaims: true,
+    loadUserInfo: true
+  };
+
+  const userManager = new UserManager(settings);
+
+  const signIn = () => {
+    userManager.signinRedirect({ state: 'some string' });
+  };
+
+  const processLoginResponse = () => {
+    userManager
+      .signinRedirectCallback()
+      .then(function(user) {
+        console.log('signed in', user);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  };
+
+  if (window.location.href.indexOf('code=') >= 0) {
+    processLoginResponse();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={signIn}>Login</button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
